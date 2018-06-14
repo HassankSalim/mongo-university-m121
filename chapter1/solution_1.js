202983f4255d6ded76410f5226e9dd00833d6864
hasali <hassan.salim@medlife.com>
2018-06-14 15:24:20 +0530

    Submit

---
+++ chapter1/solution_1.js
@@ -0,0 +1,19 @@
+var pipeline = [{ 
+    $match: {
+        'imdb.rating': {
+            $gte : 7
+        },
+        'genres' : { 
+            $nin : [ 'Crime', 'Horror' ] 
+        },
+        'rated': {
+            $in: ['PG', 'G']
+        },
+        $and : [{
+                'languages': 'English'
+            },{
+                'languages': 'Japanese'
+            }
+        ]
+    }
+}]
